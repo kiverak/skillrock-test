4e827f4d854b49b4918a927a447f893a31fe9d5e
Vitaliy Borodin <kiverak@gmail.com>
2025-08-21 20:33:15 +0300

    init

---
+++ src/test/java/com/example/skillsrocktest/SkillsrockTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skillsrocktest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillsrockTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
